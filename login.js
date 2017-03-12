8b7fb5c71a257756311daf7b04b903beeb388aa4
ALUKEM <meiaaron99@gmail.com>
2017-03-12 16:26:55 -0400

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-//def database = firebase.database();
-function writeUserData(email, uname, psw) {
-  firebase.database().ref('loginInfo/' + uname).set({
-      password: psw,
-      email: email,
-      points: ''
-  });
-}
